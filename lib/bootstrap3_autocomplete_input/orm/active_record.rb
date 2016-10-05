99803d809142e2df3ca7272376432954d2f062c5
Max Ivak <max.ivak@galacticexchange.io>
2016-10-05 11:10:09 +0300

    display_id option

--- lib/bootstrap3_autocomplete_input/orm/active_record.rb
+++ lib/bootstrap3_autocomplete_input/orm/active_record.rb
@@ -1,45 +1,56 @@
 module Bootstrap3AutocompleteInput
   module Orm
     module ActiveRecord
-      def get_autocomplete_order(method, options, model=nil)
+      def get_autocomplete_order(column_name, options, model=nil)
         order = options[:order]
 
         table_prefix = model ? "#{model.table_name}." : ""
-        order || "#{table_prefix}#{method} ASC"
+        order || "#{table_prefix}#{column_name} ASC"
       end
 
       def get_autocomplete_items(parameters)
         model   = parameters[:model]
         term    = parameters[:q] || parameters[:term]
-        method  = parameters[:method]
+
+
         options = parameters[:options]
+
+        extra_columns = (options[:extra_columns] || []).map{|c| c.to_sym}
+        #column_name  = options[:column_name]
+        column_name = parameters[:method]
+
+        #
         scopes  = Array(options[:scopes])
         where   = options[:where]
         limit   = autocomplete_option_limit(options)
-        order   = get_autocomplete_order(method, options, model)
+        order   = get_autocomplete_order(column_name, options, model)
 
         items = model.all
 
         scopes.each { |scope| items = items.send(scope) } unless scopes.empty?
 
-        items = items.select(get_autocomplete_select_clause(model, method, options)) unless options[:full_model]
-        items = items.where(get_autocomplete_where_clause(model, term, method, options)).
+        items = items.select(get_autocomplete_select_clause(model, column_name, extra_columns, options)) unless options[:full_model]
+        items = items.where(get_autocomplete_where_clause(model, term, column_name, options)).
             limit(limit).order(order)
         items = items.where(where) unless where.blank?
 
         items
       end
 
-      def get_autocomplete_select_clause(model, method, options)
+      def get_autocomplete_select_clause(model, column_name, extra_columns, options)
         table_name = model.table_name
-        (["#{table_name}.#{model.primary_key}", "#{table_name}.#{method}"] + (options[:extra_data].blank? ? [] : options[:extra_data]))
+        #cols = ([model.primary_key, column_name]+extra_columns).reject { |c| c.nil? || c.blank? || c.empty? }
+        cols = ([model.primary_key, column_name]).reject { |c| c.nil? || c.blank? || c.empty? }
+
+        #(["#{table_name}.#{model.primary_key}", "#{table_name}.#{column_name}"] + (options[:extra_data].blank? ? [] : options[:extra_data]))
+        cols.map{|c| "#{table_name}.#{c}"} + (options[:extra_data].blank? ? [] : options[:extra_data])
       end
 
-      def get_autocomplete_where_clause(model, term, method, options)
+      def get_autocomplete_where_clause(model, term, column_name, options)
         table_name = model.table_name
         is_full_search = options[:full]
         like_clause = (postgres?(model) ? 'ILIKE' : 'LIKE')
-        ["LOWER(#{table_name}.#{method}) #{like_clause} ?", "#{(is_full_search ? '%' : '')}#{term.downcase}%"]
+        ["LOWER(#{table_name}.#{column_name}) #{like_clause} ?", "#{(is_full_search ? '%' : '')}#{term.downcase}%"]
       end
 
       def postgres?(model)
